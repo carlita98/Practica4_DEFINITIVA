bf864088f02e0b327e43c7eb0975037dfcc5d28d
carlita98 <carlam13@ucm.es>
2018-03-04 12:28:50 +0100

    Commit

--- src/main/java/es/ucm/fdi/eventsBuilder/NewFaultyVehicleEventBuilder.java
+++ src/main/java/es/ucm/fdi/eventsBuilder/NewFaultyVehicleEventBuilder.java
@@ -4,6 +4,7 @@ import es.ucm.fdi.events.NewFaultyVehicleEvent;
 import es.ucm.fdi.ini.IniSection;
 
 public class NewFaultyVehicleEventBuilder {
+	
 	public Event parse(IniSection sec) {
 		if( ! sec.getTag().equals("make_vehicle_faulty")) return null;
 		return new NewFaultyVehicleEvent(sec.getValue("time"), sec.getValue("vehicles"), sec.getValue("duration"));
